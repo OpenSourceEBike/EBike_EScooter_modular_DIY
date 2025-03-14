2f16c4c4a9dadb1abef3e54805756695449289a1
casainho <casainho@gmail.com>
2025-03-14 19:15:34 +0000

    RTC now works with external module DS3231 but is optional

--- diy_display/firmware/rtc_date_time.py
+++ diy_display/firmware/rtc_date_time.py
@@ -3,55 +3,98 @@ import socketpool
 import adafruit_ntp
 import busio
 import adafruit_ds3231
-
 import rtc
 
 class RTCDateTime(object):
 
     def __init__(self, rtc_scl_pin = None, rtc_sda_pin = None):
 
-        # i2c = busio.I2C(rtc_scl_pin, rtc_sda_pin)
-        # self._rtc = adafruit_ds3231.DS3231(i2c)
-        
-        self._rtc = rtc.RTC()
+        self._rtc_external = None
 
-    def update_date_time_from_wifi_ntp(self):
-        # Get date and time from WiFi NTP
-        wifi_ssid = "HomeSweetHome"
-        wifi_password = "verygood"
-        
-        state = True
+        # Try initialize RTC external based on DS3231
+        i2c = busio.I2C(rtc_scl_pin, rtc_sda_pin)
         try:
-            wifi.radio.connect(wifi_ssid, wifi_password)    
-            if not wifi.radio.ipv4_address:  # Double-check connection
-                print("Connected to WiFi, but no IP address assigned.")
-                state = False
-            print(f"Connected to WiFi: {wifi_ssid}")
-        except ConnectionError:
-            print("Failed to connect to WiFi with provided credentials")
-            state = False
-
-        if state:
-            try:
-                pool = socketpool.SocketPool(wifi.radio)
-                ntp = adafruit_ntp.NTP(pool, tz_offset=0, cache_seconds=3600)
-                
-                # Set the RTC time
-                self._rtc.datetime = ntp.datetime
-                print(f"RTC updated to: {self._rtc.datetime}")
-                state = True
-            except Exception as e:
-                print(f"Failed to fetch time from NTP server: {e}")
-                state = False
+            self._rtc_external = adafruit_ds3231.DS3231(i2c)
+        except Exception as e:
+            print(f'Error init rtc external: {e}')
+            
+        # Initialize ESP32 internal RTC
+        self._rtc_internal = rtc.RTC()
         
-        # Wifi radio needs to reset otherwise ESPNOW will not work
-        wifi.radio.enabled = False
-        wifi.radio.enabled = True
-        
-        if state:
+    def update_internal_rtc_from_external(self):
+        # Update datetime on external RTC                    
+        if self._rtc_external is not None:
+            self._rtc_internal.datetime = self._rtc_external.datetime
+            print(f"RTC internal updated from external to: {self._rtc_internal.datetime}")
             return True
         else:
             return False
-    
+
+    def update_date_time_from_wifi_ntp(self):
+        wifi_ssid = None
+        wifi_password = None
+        ntp_datetime_state = 0
+        
+        # Get wifi ssid and password
+        import secrets
+        try:
+            wifi_ssid = secrets.secrets['wifi_ssid']
+            wifi_password = secrets.secrets['wifi_password']
+            ntp_datetime_state = 1
+        except KeyError:
+            print("Secrets are missing from secrets.py!")
+        
+        # Get date and time from WiFi NTP
+        if ntp_datetime_state == 1:
+            try:
+                wifi.radio.connect(wifi_ssid, wifi_password)  
+                if not wifi.radio.ipv4_address:  # Double-check connection
+                    print("Connected to WiFi, but no IP address assigned.")
+                
+                print(f"Connected to WiFi: {wifi_ssid}")
+                ntp_datetime_state = 2
+                
+            except ConnectionError:
+                print("Failed to connect to WiFi with provided credentials")
+                
+            # Wifi radio needs to reset otherwise ESPNOW will not work
+            wifi.radio.enabled = False
+            wifi.radio.enabled = True
+
+            if ntp_datetime_state == 2:
+                try:
+                    pool = socketpool.SocketPool(wifi.radio)
+                    ntp = adafruit_ntp.NTP(pool, tz_offset=0, cache_seconds=3600)
+                    
+                    # Set the RTC time
+                    self._rtc_internal.datetime = ntp.datetime
+                    print(f"RTC internal updated to: {self._rtc_external.datetime}")
+
+                    # Update datetime on external RTC                    
+                    if self._rtc_external is not None:
+                        self._rtc_external.datetime = self._rtc_internal.datetime
+                        print(f"RTC external updated to: {self._rtc_external.datetime}")
+
+                    ntp_datetime_state = 3
+                except Exception as e:
+                    print(f"Failed to fetch time from NTP server: {e}")
+                    if self.update_internal_rtc_from_external():
+                        ntp_datetime_state = 4     
+        else:
+            if self.update_internal_rtc_from_external():
+                ntp_datetime_state = 4
+                
+        # Return True if RTC internal has correct datetime
+        if ntp_datetime_state == 3 or ntp_datetime_state == 4:
+            return True
+        
+        elif ntp_datetime_state == 1:
+            if self.update_internal_rtc_from_external():
+                return True
+            else:
+                return False
+        else:
+            return False
+         
     def date_time(self):
-        return self._rtc.datetime
+        return self._rtc_internal.datetime
